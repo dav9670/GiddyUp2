e8b31ebd5052ebd2b96d46cdcb312b0a8ed0540b
David Lafreniere <david.lafreniere@live.ca>
2025-07-04 15:29:43 -0400

    disable GetAllowedArea

--- Source/Core/Harmony/PathUtility.cs
+++ Source/Core/Harmony/PathUtility.cs
@@ -4,38 +4,38 @@ using Verse;
 
 namespace GiddyUpCore.Core.Harmony;
 
-[HarmonyPatch(typeof(PathUtility), nameof(PathUtility.GetAllowedArea))]
-public class Patch_PathUtility_GetAllowedArea
-{
-     /// <summary>
-     /// Used to make riders avoid going into no mount zones
-     /// </summary>
-     static void Postfix(ref Area? __result, Pawn? pawn)
-     {
-         //Prepare variables and check if an area merge for riders should be done
-         if (pawn == null || !pawn.IsMounted() || pawn.Faction == null || !pawn.Faction.def.isPlayer) 
-             return;
-         var map = pawn.Map;
-         ExtendedDataStorage.Singleton.AreaNoMount.TryGetValue(map.uniqueID, out var areaNoMount);
-         if (areaNoMount == null) return;
-         var length = areaNoMount.innerGrid.arr.Length;
-
-         var invertedArea = new Area_GU(map.areaManager, ResourceBank.AreaNoMount)
-         {
-             innerGrid = new BoolGrid() { trueCountInt = areaNoMount.innerGrid.trueCountInt, arr = areaNoMount.innerGrid.arr }
-         };
-         invertedArea.Invert();
-			
-         //Merge with original passed area
-         if (__result != null)
-         {
-             var baseArr = __result.innerGrid.arr;
-             for (int i = 0; i < length; i++)
-             {
-                 if (!baseArr[i]) invertedArea.innerGrid.arr[i] = false;
-             }
-         }
-
-         __result = invertedArea;
-     }
-}
+// [HarmonyPatch(typeof(PathUtility), nameof(PathUtility.GetAllowedArea))]
+// public class Patch_PathUtility_GetAllowedArea
+// {
+//      /// <summary>
+//      /// Used to make riders avoid going into no mount zones
+//      /// </summary>
+//      static void Postfix(ref Area? __result, Pawn? pawn)
+//      {
+//          //Prepare variables and check if an area merge for riders should be done
+//          if (pawn == null || !pawn.IsMounted() || pawn.Faction == null || !pawn.Faction.def.isPlayer) 
+//              return;
+//          var map = pawn.Map;
+//          ExtendedDataStorage.Singleton.AreaNoMount.TryGetValue(map.uniqueID, out var areaNoMount);
+//          if (areaNoMount == null) return;
+//          var length = areaNoMount.innerGrid.arr.Length;
+//
+//          var invertedArea = new Area_GU(map.areaManager, ResourceBank.AreaNoMount)
+//          {
+//              innerGrid = new BoolGrid() { trueCountInt = areaNoMount.innerGrid.trueCountInt, arr = areaNoMount.innerGrid.arr }
+//          };
+//          invertedArea.Invert();
+// 			
+//          //Merge with original passed area
+//          if (__result != null)
+//          {
+//              var baseArr = __result.innerGrid.arr;
+//              for (int i = 0; i < length; i++)
+//              {
+//                  if (!baseArr[i]) invertedArea.innerGrid.arr[i] = false;
+//              }
+//          }
+//
+//          __result = invertedArea;
+//      }
+// }
